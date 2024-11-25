71e46d70b8f5835a6c0d51d3a8efb8b39e602c4b
dhy25n <sai4875@naver.com>
2024-11-25 21:27:25 +0900

    6차 - (Front) Footer 컴포넌트 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,14 @@
-import React, {useState} from 'react';
 import './App.css';
-import {commentListMock, latestBoardListMock, likeListMock, top3BoardListMock} from 'mocks';
-import Top3Item from './components/Top3Item';
-import CommentItem from './components/CommentItem';
-import LikeItem from "./components/LikeItem";
-import InputBox from "./components/InputBox";
+import React from "react";
+import Footer from "layouts/Footer";
+
 
 function App() {
 
-  const [value, setValue] = useState<string>('');
 
   return (
     <>
-        <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요.' value={value} error={true} setValue={setValue} message='error' />
+        <Footer />
     </>
   );
 }
---
+++ frontend/src/layouts/Footer/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import './style.css';
+
+/**
+ *  TODO: component: Footer 레이아웃 컴포넌트
+ * */
+export default function Footer() {
+    /**
+     *  TODO: event handler: email icon 클릭 이벤트 처리 함수
+     * */
+    const onEmailIconButtonClickHandler = () => {
+        window.location.href = 'mailto:sai4875@naver.com';
+    }
+
+    /**
+     *  TODO: event handler: github icon 클릭 이벤트 처리 함수
+     * */
+    const onGithubIconButtonClickHandler = () => {
+        window.open('https://github.com/DHye5n/admin-project');
+    }
+    /**
+     *  TODO: event handler: blog icon 클릭 이벤트 처리 함수
+     * */
+    const onBlogIconButtonClickHandler = () => {
+        window.open('https://sai4875.tistory.com/');
+    }
+
+    /**
+     *  TODO:  render: Footer 레이아웃 컴포넌트 렌더링
+     * */
+    return (
+        <div id='footer'>
+            <div className='footer-container'>
+                <div className='footer-top'>
+                    <div className='footer-logo-box'>
+                        <div className='footer-link-item'>
+                            <div className='icon-box'>
+                                <div className='icon logo-light-icon'></div>
+                            </div>
+                            <div className='footer-logo-text'>{'DHy25n Project'}</div>
+                        </div>
+                        <div className='footer-copyright'>{'Copyright ⓒ 2024 DHy25n. All Rights Reserved.'}</div>
+                    </div>
+
+                    <div className='footer-link-box'>
+                        <div className='footer-link-item'>
+                            <div className='icon-button' onClick={onEmailIconButtonClickHandler}>
+                                <div className='icon email-icon'></div>
+                            </div>
+                            <div className='footer-logo-link-text'>{'sai4875@naver.com'}</div>
+                        </div>
+
+                        <div className='footer-link-item'>
+                            <div className='icon-button' onClick={onGithubIconButtonClickHandler}>
+                                <div className='icon github-icon'></div>
+                            </div>
+                            <div className='footer-logo-link-text'>{'github.com/DHye5n/admin-project'}</div>
+                        </div>
+
+                        <div className='footer-link-item'>
+                            <div className='icon-button' onClick={onBlogIconButtonClickHandler}>
+                                <div className='icon blog-icon'></div>
+                            </div>
+                            <div className='footer-logo-link-text'>{'sai4875.tistory.com'}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
