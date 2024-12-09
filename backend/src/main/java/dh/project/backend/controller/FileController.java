a0b32439cfce9e22cfbac50bffe61b2d01da1cee
dhy25n <sai4875@naver.com>
2024-12-09 19:40:49 +0900

    18차 - (Back) 파일 업로드 작업

---
+++ backend/src/main/java/dh/project/backend/controller/FileController.java
@@ -0,0 +1,31 @@
+package dh.project.backend.controller;
+
+import dh.project.backend.dto.ApiResponseDto;
+import dh.project.backend.service.FileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/file")
+@RestController
+public class FileController {
+
+    private final FileService fileService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ApiResponseDto<String>> upload(@RequestParam("file") MultipartFile file) {
+        ApiResponseDto<String> responseDto = fileService.upload(file);
+        return ResponseEntity.status(responseDto.getStatus()).body(responseDto);
+    }
+
+    @GetMapping(value = "/{fileName}", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
+    public ResponseEntity<Resource> getFile(@PathVariable("fileName") String fileName) {
+        return fileService.getFile(fileName);
+    }
+}
---
+++ backend/src/main/java/dh/project/backend/service/FileService.java
@@ -0,0 +1,110 @@
+package dh.project.backend.service;
+
+import dh.project.backend.dto.ApiResponseDto;
+import dh.project.backend.enums.ResponseStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class FileService {
+
+    @Value("${file.path}")
+    private String filePath;
+    @Value("${file.url}")
+    private String fileUrl;
+
+    public ApiResponseDto<String> upload(MultipartFile file) {
+
+        if (file.isEmpty()) {
+            return ApiResponseDto.failure(ResponseStatus.FILE_EMPTY);
+        }
+
+        String originalFilename = file.getOriginalFilename();
+        if (StringUtils.isBlank(originalFilename)) {
+            return ApiResponseDto.failure(ResponseStatus.INVALID_FILE_NAME);
+        }
+
+        // 파일 확장자 검증
+        String extension = originalFilename.substring(originalFilename.lastIndexOf(".")).toLowerCase();
+
+        // 허용된 확장자 목록 설정 (예: 이미지 파일만 허용)
+        String[] allowedExtensions = {".png", ".jpg", ".jpeg"};
+        boolean isValidExtension = false;
+
+        for (String ext : allowedExtensions) {
+            if (extension.equals(ext)) {
+                isValidExtension = true;
+                break;
+            }
+        }
+
+        if (!isValidExtension) {
+            return ApiResponseDto.failure(ResponseStatus.INVALID_FILE_EXTENSION);
+        }
+
+        // 저장할 파일 이름 생성
+        String uuid = UUID.randomUUID().toString();
+        String saveFileName = uuid + extension;
+        String savePath = Paths.get(filePath, saveFileName).toString();
+
+        // 파일 디렉토리 존재 여부 확인 및 생성
+        File directory = new File(filePath);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        try {
+            // 파일 저장
+            file.transferTo(new File(savePath));
+        } catch (IOException e) {
+            log.error("Error during file upload: ", e);
+            return ApiResponseDto.failure(ResponseStatus.FILE_UPLOAD_FAIL);
+        }
+
+        String url = String.format("%s/%s", fileUrl.replaceAll("/$", ""), saveFileName);
+
+        return ApiResponseDto.success(ResponseStatus.SUCCESS, url);
+    }
+
+    public ResponseEntity<Resource> getFile(String fileName) {
+
+        // 파일 경로 설정
+        String filePath = Paths.get(this.filePath, fileName).toString();
+        File file = new File(filePath);
+
+        log.info("File path: {}", filePath);
+
+        // 파일 존재 여부 확인
+        if (!file.exists()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        // 파일 읽기
+        try {
+            Resource resource = new FileSystemResource(file);
+            String mimeType = Files.probeContentType(file.toPath());
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(mimeType))
+                    .body(resource);
+        } catch (IOException e) {
+            log.error("Error during file read: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
