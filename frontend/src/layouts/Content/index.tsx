a43045049a9b3a80714f30025c9c28591bf476aa
dhy25n <sai4875@naver.com>
2024-12-11 21:04:48 +0900

    20-3차 - (Front) 이미지 박스 재작업

--- frontend/src/layouts/Content/index.tsx
+++ frontend/src/layouts/Content/index.tsx
@@ -1,10 +1,18 @@
 import './style.css';
 import BoardWrite from 'views/Board/Write';
+import { useLocation } from 'react-router-dom';
+import { BOARD_WRITE_PATH, MAIN_PATH } from '../../constant';
 
 export default function Content() {
+  /**
+   *  TODO:  state: 현재 페이지 path name 상태
+   * */
+  const { pathname } = useLocation();
+
   return (
     <div id='content'>
-        <BoardWrite />
+      {pathname.startsWith('/board/write') && <BoardWrite />}
+      {pathname === MAIN_PATH() && <div>메인페이지</div>}
     </div>
   );
 }
