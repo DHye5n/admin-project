6a958fc12f00d1e789b1b8a6b4b067e72d1cc59d
dhy25n <sai4875@naver.com>
2025-02-03 16:17:03 +0900

    41차 - 정적파일 업로드

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // React 17+에서는 JSX 범위 필요 없음
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off", // 확실히 끔
+    },
+  },
+];
